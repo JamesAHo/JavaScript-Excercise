21f4367797c5e3ee4f6d557c04a8a90bba8fa18c
James Anh <james.anh184@gmail.com>
2022-11-07 13:24:57 -0800

    prototype

--- 01_02 Movie Object/begin.js
+++ 01_02 Movie Object/begin.js
@@ -1,3 +1,18 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+function Movie(title, director, genre, releaseYear, rating) {
+  this.title = title;
+  this.director = director;
+  this.genre = genre;
+  this.releaseYear = releaseYear;
+  this.rating = rating;
+}
+Movie.prototype.getOverview = function() {
+  return ` This Movie${this.title} directed by ${this.director} was released 
+  in ${this.releaseYear}. It received a rating of ${this.rating}`
+}
+
+const Minions = new Movie("James the Great", "Jenny Nga", "Action", 2022, 10)
+console.log(Minions)
+console.log(Minions.getOverview())
