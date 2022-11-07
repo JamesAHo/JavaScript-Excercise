43d3913a4123aa8e974f99fec3720827652442d9
James Anh <james.anh184@gmail.com>
2022-11-07 13:46:02 -0800

    Vegetarian dinner filter method

--- 01_03 Vegetarian Dinner/begin.js
+++ 01_03 Vegetarian Dinner/begin.js
@@ -1,3 +1,29 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+const menu = [
+  {
+    name: "Chicken parmesan",
+    isVegetarian: false,
+  },
+  {
+    name: "Penne a la vodka",
+    isVegetarian: true,
+  },
+  {
+    name: "Mushroom risotto",
+    isVegetarian: true,
+  },
+  {
+    name: "Veal saltambuca",
+    isVegetarian: false,
+  },
+  {
+    name: "Filet mignon",
+    isVegetarian: false,
+  },
+];
+
+const vegetearian = menu.filter(veg => veg.isVegetarian === true)
+
+console.log(vegetearian);
