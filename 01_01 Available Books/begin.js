4df98feb86f91c69265e3ef9da702a1dd6a657c7
James Anh <james.anh184@gmail.com>
2022-11-07 12:45:27 -0800

    set up book constrcutor

--- 01_01 Available Books/begin.js
+++ 01_01 Available Books/begin.js
@@ -1,3 +1,29 @@
 /*
-  WRITE YOUR SOLUTION HERE
+  Books: Title, Author, ISBN, numCopies
+  getAvailability() with statement if === 0 > out of stock || if <10 === low stock otherwise in stock
+  sell(numSold) 
+  restock(numCopies)
 */
+
+class Books {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+  // getter 
+  get availability() {
+    return this.getAvailability();
+  }
+  // begin methods
+  getAvailability() {
+    if(this.numCopies === 0) {
+      return "Out of stock";
+    } else if (this.numCopies < 10) {
+      return "Low in stock";
+    }
+    return "In stock";
+  }
+
+}
