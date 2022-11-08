0dc435aa8946aceed48afe2a59ec934449cd2baa
James Anh <james.anh184@gmail.com>
2022-11-07 22:13:48 -0800

    implemented closures

--- 01_08 Dynamic Colors/begin.js
+++ 01_08 Dynamic Colors/begin.js
@@ -1,3 +1,18 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+// closure is the combination of a function bundled together with regerences to its surrounding state(lexical enviorment
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+
+function switchColor(color) {
+  return function () {
+    document.body.style.backgroundColor = color
+  }
+};
+const colorBlue = switchColor("blue");
+const colorRed = switchColor("red");
+const colorGreen = switchColor("green");
+
+document.querySelector("#blue").addEventListener("click", colorBlue);
+document.querySelector("#pink").addEventListener("click", colorRed);
+document.querySelector("#green").addEventListener("click", colorGreen);
