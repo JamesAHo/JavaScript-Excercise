34a6893de58a0c8e5628d4d2b61f96d653b6064f
James Anh <james.anh184@gmail.com>
2022-11-07 21:19:20 -0800

    private object using Symbol()

--- 01_05 Private Object/begin.js
+++ 01_05 Private Object/begin.js
@@ -1,3 +1,21 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol
+// Symbol is a built-in object whose constructor returns a symbol primitives
+// also called Symbol value or just a symbol that's guaranteed to be unique.
+
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username]: "james",
+  [password]: "33@nfsdjnfsd",
+  age:27
+};
+
+console.log(user.username)
+console.log(user.password)
+
+// output : undefined
+
