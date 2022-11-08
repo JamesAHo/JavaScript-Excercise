0cf35e74a494bde340a599af3bdb458ed963b765
James Anh <james.anh184@gmail.com>
2022-11-07 21:38:16 -0800

    call back function

--- 01_06 Ticking Time/begin.js
+++ 01_06 Ticking Time/begin.js
@@ -1,3 +1,17 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+function Clock() {
+  const clockNode = document.querySelector("#clock");
+  return setInterval(() => {
+    // get date
+    let date = new Date();
+    let time = date.toLocaleTimeString();
+    // render time
+    clockNode.textContent = time;
+  }, 1000);
+}
+
+// call ClickingClock()
+Clock();
